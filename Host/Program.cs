abfd1e0837e6f42e4efc0dd038b0491d2b1b9399
Breno Gomes Haese <bghaesee@outlook.com>
2025-07-06 11:48:25 -0300

    🧱 chore: save db and cache with bind mount

--- Host/Program.cs
+++ Host/Program.cs
@@ -13,19 +13,22 @@ IResourceBuilder<ParameterResource> cachePassword = builder.AddParameter("cache-
 IResourceBuilder<PostgresServerResource>   pg = builder.AddPostgres("pg", dbUsername, dbPassword);
 IResourceBuilder<PostgresDatabaseResource> db = pg.AddDatabase("db");
 
-IResourceBuilder<RedisResource> cache = builder.AddRedis("cache", 6379,cachePassword);
+IResourceBuilder<RedisResource> cache = builder.AddRedis("cache", 6379, cachePassword);
 
 IResourceBuilder<ProjectResource> api = builder.AddProject<Projects.Api>("backend", options => {
 	options.ExcludeKestrelEndpoints = true;
 });
 
 cache.WithImage("library/redis", "8-alpine");
-cache.WithHostPort(6379);
 cache.WithLifetime(ContainerLifetime.Persistent);
+cache.WithDataBindMount(source: "./data/cache");
+
+cache.WithHostPort(6379);
 
 pg.WithImage("library/postgres", "17-alpine");
-pg.WithHostPort(5432);
 pg.WithLifetime(ContainerLifetime.Persistent);
+pg.WithDataBindMount(source: "./data/db");
+pg.WithHostPort(5432);
 
 api.WithEnvironment("discord-token", discordToken);
 api.WithReference(cache);
