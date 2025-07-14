5fd645796c41bc4c221b74eb98af366a6e01dc9d
Breno Gomes Haese <bghaesee@outlook.com>
2025-07-14 06:12:32 -0300

    🗃️ chore: run migrations

--- Shared/Migrations/20250714072105_add_placement_to_match_player.Designer.cs
+++ Shared/Migrations/20250714072105_add_placement_to_match_player.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Shared.Data;
@@ -11,9 +12,11 @@ using Shared.Data;
 namespace Shared.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250714072105_add_placement_to_match_player")]
+    partial class add_placement_to_match_player
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
